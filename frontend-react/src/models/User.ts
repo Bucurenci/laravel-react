708ec3f49ba74f31a93202fa07123b817c3e9373
slymer <slymer24@gmail.com>
2023-10-02 13:14:46 +0300

    [frontend]: Implemented MUI (part 10) - UserCreateForm

--- frontend-react/src/models/User.ts
+++ frontend-react/src/models/User.ts
@@ -94,14 +94,6 @@ interface UserAvatar {
     thumb: string;
 }
 
-export interface NewUser {
-    first_name: string;
-    last_name: string;
-    email: string;
-    password: string;
-    password_confirmation: string;
-}
-
 export interface User {
     id: number;
     first_name: string;
